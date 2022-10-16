d71dd544cb99907e617910cf025c62a6ae11b116
steven07 <67178510+StevSteven07@users.noreply.github.com>
2022-10-16 16:40:42 -0500

    se sube de nuevo

--- src/main/java/Reto4/despliegue/controller/AdminController.java
+++ src/main/java/Reto4/despliegue/controller/AdminController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/Admin") --
+@RequestMapping("/api/Admin") 
 public class AdminController {
 
     @Autowired
